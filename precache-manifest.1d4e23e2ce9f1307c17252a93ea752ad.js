827d14f85ac4141f67eb7f39c4b20a8aa865b783
flamekaizerr <kkhalidkhan23@gmail.com>
2023-03-30 23:08:45 +0530

    Updates

---
+++ precache-manifest.1d4e23e2ce9f1307c17252a93ea752ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fb8b32d1e1192c4c228",
+    "url": "/GoogleKeepClone/static/css/main.4a265546.chunk.css"
+  },
+  {
+    "revision": "4fb8b32d1e1192c4c228",
+    "url": "/GoogleKeepClone/static/js/main.4fb8b32d.chunk.js"
+  },
+  {
+    "revision": "2b57abdb138a9f43617f",
+    "url": "/GoogleKeepClone/static/js/1.2b57abdb.chunk.js"
+  },
+  {
+    "revision": "8cd5099ae0cfc40dab29",
+    "url": "/GoogleKeepClone/static/js/runtime~main.8cd5099a.js"
+  },
+  {
+    "revision": "6914e4ef8e4f39fdb35d3fe42e0cce71",
+    "url": "/GoogleKeepClone/index.html"
+  }
+];
